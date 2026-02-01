7833fdd43dbda1da12d4b6dea8e78e71b871f395
lalatlangau <163626123+lalatlangau@users.noreply.github.com>
2026-02-01 10:31:56 +0800

    Update AnimeIndo.kt

--- AnimeIndo/src/main/kotlin/com/animeindo/AnimeIndo.kt
+++ AnimeIndo/src/main/kotlin/com/animeindo/AnimeIndo.kt
@@ -11,7 +11,7 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 
 class AnimeIndo : MainAPI() {
-    override var mainUrl = "https://animeindo.xyz"
+    override var mainUrl = "https://samehadaku.biz"
     override var name = "AnimeIndo"
     override val hasMainPage = true
     override var lang = "id"
